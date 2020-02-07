b1db88858a5cb71128d25043419878c58ffbefda
ahallam <alexhallam75@gmail.com>
2020-02-07 16:20:30 -0500

    Attack button connected to calculateDamage function

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ new Vue({
       }
 
       this.monsterAttacks();
+    },
+    calculateDamage: function(min, max) {
+      return Math.max(Math.floor(Math.random() * max) + 1, min);
     }
   }
 });
